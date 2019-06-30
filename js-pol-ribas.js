c0c017701231496d9767f51e7d843fdcfd29fc39
Pol Ribas <polribasrovira@gmail.com>
2019-06-30 21:58:14 +0200

    pequeña actualizacion de contenido

--- js-pol-ribas.js
+++ js-pol-ribas.js
@@ -1,16 +1,28 @@
 'use strict';
 
 function main() {
+    // ***************** Project Variable *****************
     var flechaProjects = document.querySelector('.flecha-projects');
     var itemProjects = document.querySelectorAll('.card-item');
-    var rehacer = document.querySelector('.titulo-projects');
+    var rehacerProyectos = document.querySelector('.titulo-projects');
 
+    // ***************** Project Laboratoris *****************
+    var flechaLaboritoris = document.querySelector('.flecha-laboratoris');
+    var itemLaboritoris = document.querySelectorAll('.card-item-laboratoris');
+    var rehacerLaboritoris = document.querySelector('.titulo-laboratoris');
+
+    // ***************** Events Variable *****************
+    flechaProjects.addEventListener('click', desplegarProyectos);
+    flechaLaboritoris.addEventListener('click', desplegarLaboritoris);
+
+
+    // ***************** Funcions Project *****************
     function desplegarProyectos() {
         itemProjects.forEach(function(articulo) {
             articulo.classList.remove('escondido');
         });
         flechaProjects.classList.add('escondido');
-        rehacer.addEventListener('click', replegarProyectos);
+        rehacerProyectos.addEventListener('click', replegarProyectos);
         flechaProjects.removeEventListener('click', desplegarProyectos);
     }
 
@@ -20,10 +32,27 @@ function main() {
         });
         flechaProjects.classList.remove('escondido');
         flechaProjects.addEventListener('click', desplegarProyectos);
-        rehacer.removeEventListener('click', replegarProyectos);
+        rehacerProyectos.removeEventListener('click', replegarProyectos);
     }
 
-    flechaProjects.addEventListener('click', desplegarProyectos);
+    // ***************** Funcions Laboratoris *****************
+    function desplegarLaboritoris() {
+        itemLaboritoris.forEach(function(articulo) {
+            articulo.classList.remove('escondido');
+        });
+        flechaLaboritoris.classList.add('escondido');
+        rehacerLaboritoris.addEventListener('click', replegarLaboritoris);
+        flechaLaboritoris.removeEventListener('click', desplegarLaboritoris);
+    }
+
+    function replegarLaboritoris() {
+        itemLaboritoris.forEach(function(articulo) {
+            articulo.classList.add('escondido');
+        });
+        flechaLaboritoris.classList.remove('escondido');
+        flechaLaboritoris.addEventListener('click', desplegarLaboritoris);
+        rehacerLaboritoris.removeEventListener('click', replegarLaboritoris);
+    }
 
 }
 
